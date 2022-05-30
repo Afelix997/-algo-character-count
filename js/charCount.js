f8c57d49f6ac207a35aba87f175b3bad8ccfcd38
afelix997 <afelix2013@gmail.com>
2022-05-30 10:50:30 -0700

    answer

--- js/charCount.js
+++ js/charCount.js
@@ -1,3 +1,16 @@
-exports.charCount = function() {
-
+exports.charCount = function(string) {
+    let obj={}
+    let resArr;
+    string= string.toString().replace(/[^a-zA-Z0-9]/g,"").toLowerCase().split("").sort()
+    string.forEach(value => {
+        if (obj[value]){
+        obj[value] +=1
+    }else{
+        obj[value]=1
+    }})
+    resArr= Object.entries(obj)
+    resArr.sort((a, b)=> {
+    return b[1] - a[1]
+})
+    return resArr
 };
