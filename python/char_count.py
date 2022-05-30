6fa0321a94d8ae875fbc1fa464e10f2fe595601a
afelix997 <afelix2013@gmail.com>
2022-05-30 14:20:28 -0700

    answer

--- python/char_count.py
+++ python/char_count.py
@@ -1,2 +1,14 @@
 def char_count(str):
-  # Your code here
+  obj= {}
+  res_arr=[]
+  input_arr= list(input.replace(" ", "").lower())
+  for value in input_arr:
+    if value in obj.keys():
+      obj[value]+=1
+    else:
+      obj[value]=1
+  for key in obj:
+    res_arr.append([key,obj[key]])
+  
+  return sorted(res_arr, key=lambda x : (-x[1], x[0]))
+         
